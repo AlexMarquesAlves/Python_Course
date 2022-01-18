db6e7ef68ef7638f91f38335ec4cd41953c2552c
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-18 16:13:28 -0300

    Variáveis

---
+++ aula6/aula6.py
@@ -0,0 +1,19 @@
+'''
+Iniciar com letra, pode conter números , separar _, letras minusculas
+'''
+nome = 'Leonardo Marques'
+idade = 22
+altura = 1.85
+e_maior = idade > 18
+data_atual = 2022
+peso = 70
+imc = peso / altura ** 2
+
+
+# print('Nome:', nome)
+# print('Idade:', idade)
+# print('Altura:', altura)
+# print('É maior:', e_maior)
+
+print(nome, 'tem', idade, 'anos de idade e seu imc é', imc)
+
