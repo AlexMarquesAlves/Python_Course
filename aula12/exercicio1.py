2e2297c82de569389d5aa9631d66459d891fbe66
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-31 16:18:44 -0300

    Exercício 1, Terminado!

--- aula12/exercicio1.py
+++ aula12/exercicio1.py
@@ -3,3 +3,13 @@ Faça um programa que peça ao usuario para digitar um número inteiro,
 informe se este numero é par ou ímpar. Caso o usuario não digite um número
 inteiro, informe que não é um número inteiro.
 """
+
+number = input('Digite um número Inteiro: ')
+if number.isnumeric():
+    number = int(number)
+    if (number % 2) == 0:
+        print('Seu número é Par. ')
+    else:
+        print('Seu número é ímpar. ')
+else:
+    print("...Por Favor Digite um número Válido. ")
