1348f2e40c7d7d8d644ea21c6c706f8e2046942d
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-01 16:10:33 -0300

    Expressão condicional com operador OR

---
+++ aula026/aula26.py
@@ -0,0 +1,17 @@
+
+nome = input('Qual seu nome? ')
+
+if nome:
+    print(nome)
+else:
+    print('Você não digitou nada =(')
+
+print()
+Operador = 'Operador OR'
+print(f'{Operador:-^35}')
+nome = input('Qual seu nome? ')
+
+print(nome or 'Você não digitou nada! ')
+print()
+ex = 'Exemplo'
+print(f'{ex:-^35}')
---
+++ aula026/exemplo_1.py
@@ -0,0 +1,15 @@
+ex = 'Exemplo'
+print(f'{ex:-^35}')
+print()
+
+a = 0
+b = None
+c = False
+d = []
+e = {}
+f = 22
+g = 'Luiz'
+
+variavel = a or b or c or d or e or f or g
+
+print(variavel)
