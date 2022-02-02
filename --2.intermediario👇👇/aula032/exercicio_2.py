ec395122cceedea946446021f31a31c098464411
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-01 21:40:06 -0300

    exercicio_2 resolução minha e do curso

---
+++ --2.intermediario👇👇/aula032/exercicio_2.py
@@ -0,0 +1,31 @@
+print("-" * 12, "Exercício_2", "-" * 12)
+"""
+2 - Crie uma função que recebe 3 números com parameters e exiba a soma entre eles.
+"""
+
+
+def soma(n1, n2, n3):
+
+    print(n1 + n2 + n3)
+
+
+print()
+soma(2, 1, 3)
+soma(10, 10, 31)
+soma(26, 11, 15)
+soma(13, 32, 23)
+soma(25, 27, 12)
+
+print("-" * 8, "Resultado no curso", "-" * 8)
+print()
+
+
+def soma(n1, n2, n3):
+    print(n1 + n2 + n3)
+
+
+soma(2, 1, 3)
+soma(1, 1, 1)
+soma(2, 1, 1)
+soma(3, 3, 3)
+soma(2, 2, 2)
