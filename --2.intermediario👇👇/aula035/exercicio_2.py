6e40d939b98718543264fe5d37e083c7b1dee7b9
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-02 17:07:13 -0300

    Exercício_2 - Aula 35

---
+++ --2.intermediario👇👇/aula035/exercicio_2.py
@@ -0,0 +1,27 @@
+print('-' * 12, 'Exercise_2', '-' * 12)
+"""
+2 - Crie uma função-1 que recebe uma função-2 com parâmetro e retorne o valor da
+função executada. faça a função-1 executar duas funções que recebam um número
+diferente de argumentos.
+"""
+
+
+def master(func, *args, **kwargs):
+    return func(*args, **kwargs)
+
+
+print()
+
+
+def say_hello(name):
+    return f'Hello {name}!'
+
+
+def greeting(name, salutation):
+    return f'{salutation}! {name}.'
+
+
+running = master(say_hello, 'Leo')
+running2 = master(greeting, 'Leo', salutation='Good Day')
+print(running)
+print(running2)
