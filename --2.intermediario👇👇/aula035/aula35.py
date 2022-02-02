1ca4e80980a26bdbf8cbd9836cb626e3202738e6
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-02 16:45:27 -0300

    Exercícios propostos

---
+++ --2.intermediario👇👇/aula035/aula35.py
@@ -0,0 +1,11 @@
+print()
+"""
+1 - Crie uma função que recebe uma função-2 como parâmetro e retorne o valor da
+função-2 executada.
+"""
+
+"""
+2 - Crie uma função-1 que recebe uma função-2 com parâmetro e retorne o valor da
+função executada. faça a função-1 executar duas funções que recebam um número
+diferente de argumentos.
+"""
