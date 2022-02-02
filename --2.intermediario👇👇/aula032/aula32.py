2c00fb86c0094aa213da3a2c897309bc2a77793a
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-01 21:03:03 -0300

    Exercícios propostos

---
+++ --2.intermediario👇👇/aula032/aula32.py
@@ -0,0 +1,21 @@
+print("-"*12, "Exercícios", "-"*12)
+"""
+1 - Crie uma função que exiba uma saudação com os parameters saudação e nome.
+"""
+
+"""
+2 - Crie uma função que recebe 3 números com parameters e exiba a soma entre eles.
+"""
+
+"""
+3 - Crie uma função que receba 2 números. O primeiro é um valor e o segundo um 
+percentual (ex. 10%). Retorne (return) o valor do primeiro número somado
+do aumento do percentual do mesmo.
+"""
+
+"""
+4 - Fizz Buzz - Se o parameter da função for divisíval por 2, retorne fizz,
+se o parameter da função for divisível por 5, retorne buzz. Se o parameter da
+função for divisível por 5 e por 3, retorne FizzBuzz, caso contrário retorne o
+número enviado
+"""
