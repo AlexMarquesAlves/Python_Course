f11c7cf82b6637f806a7971ede296e563a3b065d
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-11 19:59:20 -0300

    aula 52

---
+++ --2.intermediario👇👇/aula052/aula52.py
@@ -0,0 +1,8 @@
+print()
+'''
+Combinations, Permutations e Products - Itertools
+Combinação - Ordem não importa
+Permutação - Ordem importa
+Ambos não repetem valores únicos
+Produtos - Ordem importa e repete valores únicos
+'''
