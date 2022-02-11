a63b853e74738d266d217671ed12673b5dbec8aa
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-11 19:46:37 -0300

    Count - Contadores infinitos

--- --2.intermediario👇👇/aula051/aula51.py
+++ --2.intermediario👇👇/aula051/aula51.py
@@ -12,3 +12,31 @@ print(isinstance(variavel, GeneratorType))
 """
 Count - Itertools
 """
+print('#'*30)
+from itertools import count
+
+contador = count(start=5, step=.1) # Causa um loop infinito quando usado num loop for
+
+for valor in contador:
+    print(round(valor,2))
+
+    if valor >= 10:
+        break
+
+print('#'*30)
+from itertools import count
+
+contador = count(start=-1, step=-1) # Causa um loop infinito quando usado num loop for
+
+for valor in contador:
+    print(round(valor,2))
+
+    if valor >= 10 or valor <=-10:
+        break
+
+print('#'*30)
+
+contador = count()
+lista = ['Luiz','João', 'Maria']
+lista = zip(contador, lista)
+print(list(lista))
