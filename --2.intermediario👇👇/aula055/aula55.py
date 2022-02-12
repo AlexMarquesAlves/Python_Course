72ae2bc516c3642bb16b6c39b38a5a210077f57a
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-11 23:11:26 -0300

    Filtrando os dados de Produtos

--- --2.intermediario👇👇/aula055/aula55.py
+++ --2.intermediario👇👇/aula055/aula55.py
@@ -1,5 +1,12 @@
 from dados import produtos, pessoas, lista
 
+print('----> Filtrando os dados da lista com List Comprehension')
 # nova_lista = filter(lambda x: x > 5, lista)
 nova_lista = [x for x in lista if x > 5]
 print(list(nova_lista))
+print('----> Filter >> Produtos')
+
+nova_lista = filter(lambda p: p['preco'] > 50, produtos)
+
+for produto in nova_lista:
+    print(produto)
