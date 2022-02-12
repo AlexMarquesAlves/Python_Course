9a9bc66ca85fb5926ca0f14116a23929ca42fd58
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-11 22:36:50 -0300

    Mapeamento de dicionários

--- --2.intermediario👇👇/aula054/mapeamento.py
+++ --2.intermediario👇👇/aula054/mapeamento.py
@@ -1,6 +1,12 @@
 from dados import produtos, pessoas, lista
 
+print('----> Mapeamento de lista com map() e list comprehension')
 # nova_lista = map(lambda x: x*2, lista)
 nova_lista = [x * 2 for x in lista]
 print(lista)
 print(list(nova_lista))
+print()
+print('----> Mapeamento de dicionários')
+
+for produto in produtos:
+    print(produto)
