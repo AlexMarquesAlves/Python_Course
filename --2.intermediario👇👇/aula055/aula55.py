2498194d82acc696beaa2b4fbe407684bee7b328
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-11 23:18:32 -0300

    Adicionando nova chave ao dicionário por uma função

--- --2.intermediario👇👇/aula055/aula55.py
+++ --2.intermediario👇👇/aula055/aula55.py
@@ -9,9 +9,9 @@ print('----> Filter >> Produtos')
 
 def filtra(produto):
     if produto['preco'] > 50:
-        return True
-    else:
-        return False
+        produto['e_caro'] = True
+    return True
+
 
 
 nova_lista = filter(filtra, produtos)
