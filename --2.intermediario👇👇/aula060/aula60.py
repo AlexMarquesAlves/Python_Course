e3e0ce6b7314d9c2b2b457697e80049d4c26ac67
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-12 23:05:18 -0300

    exemplo módulo sys

---
+++ --2.intermediario👇👇/aula060/aula60.py
@@ -0,0 +1,8 @@
+# Módulos padrão do Python
+# Módulos são arquivos .py (que contem código python) e servem para expandir
+# as funcionalidades padrão da linguagem.
+# Veja todos os módulos padrão do python em:
+# https://docs.python.org/3/py-modindex.html
+from sys import platform as so  # módulo sys
+print('Exemplo módulo - sys')
+print(so)
