50ef3a1cd781f5317de1ebe03a8e40429d846188
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-12 23:22:54 -0300

    instalando módulos externos no Python

--- --2.intermediario👇👇/aula060/aula60.py
+++ --2.intermediario👇👇/aula060/aula60.py
@@ -4,5 +4,20 @@
 # Veja todos os módulos padrão do python em:
 # https://docs.python.org/3/py-modindex.html
 from sys import platform as so  # módulo sys
-print('Exemplo módulo - sys')
+from random import randint as randint_original, random
+import pymysql  # pip install pymysql
+
+print(f'\t\tExemplo módulo - sys')
 print(so)
+
+print(f'\t\tExemplo módulo - random')
+
+
+def randint(*args):  # sobrescreve o módulo original...
+    return 'hahaha'
+
+
+for i in range(10):
+    print(randint_original(0, 10), random())
+
+print(randint())
