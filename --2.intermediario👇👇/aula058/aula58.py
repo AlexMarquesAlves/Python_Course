54d9c2371c0f8d3f329b637ced856dc259c3ed12
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-12 22:21:56 -0300

    Levantando propria exceção

--- --2.intermediario👇👇/aula058/aula58.py
+++ --2.intermediario👇👇/aula058/aula58.py
@@ -1,14 +1,9 @@
 # https://docs.python.org/3/library/exceptions.html
 
 def divide(n1, n2):
-    try:
-        return n1 / n2
-    except ZeroDivisionError as error:
-        print(error)
-        raise  # relança a exceção
+    if n2 == 0:
+        raise ValueError('n2 não pode ser 0.')
+    return n1 / n2
 
 
-try:
-    print(divide(2, 0))
-except ZeroDivisionError as error:
-    print(error)
+print(divide(2, 0))
