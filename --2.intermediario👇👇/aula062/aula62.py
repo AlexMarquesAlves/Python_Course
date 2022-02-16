d8ad2b268188c8ecf1d282164d641bb20a1316e9
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-16 00:28:30 -0300

    Melhorando ainda mais a maneira de importar

--- --2.intermediario👇👇/aula062/aula62.py
+++ --2.intermediario👇👇/aula062/aula62.py
@@ -1,6 +1,9 @@
 
-from vendas import calc_precos
+from vendas.calc_precos import aumento, reducao
 
 price = 49.90
-price_com_aumento = calc_precos.aumento(valor=price, porcentagem=15)
+price_com_aumento = aumento(valor=price, porcentagem=15)
 print(price_com_aumento)
+
+price_com_reducao = reducao(valor=price, porcentagem=15)
+print(price_com_reducao)
