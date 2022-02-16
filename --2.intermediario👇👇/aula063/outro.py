6f7c5f469db2a957d70f6402e59474000e3bebb8
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-16 01:17:18 -0300

    Gerenciadores de contexto

--- --2.intermediario👇👇/aula063/outro.py
+++ --2.intermediario👇👇/aula063/outro.py
@@ -7,3 +7,13 @@ try:
     print(file.read())
 finally:
     file.close()
+
+print('########################')
+
+with open('abc.txt', 'w+') as file:
+    file.write('Linha 1\n')
+    file.write('Linha 2\n')
+    file.write('Linha 3\n')
+
+    file.seek(0)
+    print(file.read())
