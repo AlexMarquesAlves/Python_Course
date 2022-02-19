04ed101841b629262f47cd7b76694ac0bb2e0e1b
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-19 17:46:27 -0300

    Aula 69 — Criando função que Formata CNPJ gerado

--- --2.intermediario👇👇/aula069/cnpj.py
+++ --2.intermediario👇👇/aula069/cnpj.py
@@ -66,9 +66,15 @@ def gera():
     quarto_bloco = '0001'
 
     inicio_cnpj = f'{primeiro_digito}{segundo_digito}{segundo_bloco}' \
-        f'{terceiro_bloco}{quarto_bloco}00'
+                  f'{terceiro_bloco}{quarto_bloco}00'
 
     novo_cnpj = calcula_digito(cnpj=inicio_cnpj, digito=1)
     novo_cnpj = calcula_digito(cnpj=novo_cnpj, digito=2)
 
     return novo_cnpj
+
+
+def formata(cnpj):
+    cnpj = apenas_numeros(cnpj)
+    formatado = f'{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:14]}'
+    return formatado
