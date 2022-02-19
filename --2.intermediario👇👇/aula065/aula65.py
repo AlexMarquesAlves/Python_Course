535a09c5df3d373dfe90843c64fe27eefc98fafb
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-19 15:46:37 -0300

    Aula 65 — Funções decoradoras e decoradores

--- --2.intermediario👇👇/aula065/aula65.py
+++ --2.intermediario👇👇/aula065/aula65.py
@@ -1,3 +1,7 @@
+from time import time
+from time import sleep
+
+
 # Funções como variáveis
 def fala_oi():
     print('Oi')
@@ -103,3 +107,38 @@ def fala_oi(msg):
 
 
 fala_oi('Olá, sou Luiz')
+
+
+def velocidade(funcao):
+    """
+    Função decoradora: Verifica o tempo que uma função leva para executar
+    """
+
+    def envolve(*args, **kwargs):
+        """ Função que envolve e executa outra função """
+        # Tempo inicial
+        start = time()
+        # Executa a função
+        resultado = funcao(*args, **kwargs)
+        # Tempo final
+        end = time()
+        # Resultado de tempo em ms
+        tempo = (end - start) * 1000
+        # Mostra o tempo
+        print(f'\nA função levou {tempo:.2f}ms para ser executada.')
+        # Retorna a função original executada
+        return resultado
+
+    # Retorna a função que envolve
+    return envolve
+
+
+@velocidade
+def demora(qtd):
+    """ Função decorada """
+    for i in range(qtd):
+        print(i, end='')
+
+
+# Executa a função decorada
+demora(10000)
