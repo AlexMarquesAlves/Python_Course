24f8de480ac4e0e4dbfd38a6ed7e34a9ae8ab14f
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-02-19 15:39:29 -0300

    Aula 65 — Uma função dentro de outra

--- --2.intermediario👇👇/aula065/aula65.py
+++ --2.intermediario👇👇/aula065/aula65.py
@@ -7,3 +7,16 @@ def fala_oi():
 variavel = fala_oi
 print(type(variavel))  # function
 variavel()  # Oi
+
+# Uma função dentro de outra
+def master():
+    # Função interna
+    def slave():
+        print('Oi')
+    # Função a ser executada
+    return slave
+
+# Variável recebe função
+variavel = master()
+# Executa a função interna de master
+variavel()
