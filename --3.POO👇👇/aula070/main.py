3aacdff865033daa6cf932b57942ca30fc8ae945
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-17 19:04:07 -0300

    Feat: Atribuindo direto na classe

--- --3.POO👇👇/aula070/main.py
+++ --3.POO👇👇/aula070/main.py
@@ -1,17 +1,22 @@
 from pessoa import Pessoa
 
-p1 = Pessoa()
-p2 = Pessoa()
+# p1 = Pessoa()
+# p2 = Pessoa()
+#
+# print()
+# print(f'P1 é != de P2')
+# print(p1)
+# print(p2)
+# print()
+# # Atributo nome na classe
+# p1.nome = 'Luiz'
+# p2.nome = 'João'
+# print(p2.nome)
+# print(f'-' * 20)
 
-print()
-print(f'P1 é != de P2')
-print(p1)
-print(p2)
-print()
-# Atributo nome na classe
-p1.nome = 'Luiz'
-p2.nome = 'João'
-print(p2.nome)
-print(f'-' * 20)
+# Atribuindo direto na classe
+
+p1 = Pessoa(f'Luiz, {29}')
 
 
+# p2 = Pessoa(f'João, {32}')
