e54437d896a7ee2ad7ce9f4d52adb361deeb1235
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-17 19:03:36 -0300

    Feat: Atributo nome na classe

---
+++ --3.POO👇👇/aula070/main.py
@@ -0,0 +1,17 @@
+from pessoa import Pessoa
+
+p1 = Pessoa()
+p2 = Pessoa()
+
+print()
+print(f'P1 é != de P2')
+print(p1)
+print(p2)
+print()
+# Atributo nome na classe
+p1.nome = 'Luiz'
+p2.nome = 'João'
+print(p2.nome)
+print(f'-' * 20)
+
+
